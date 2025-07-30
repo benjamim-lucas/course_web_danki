4e37fe15b7a2840cf93667468a18e8c3d84c27b4
Ben <benjamimlucasrodrigues@gmail.com>
2025-07-29 21:16:41 -0300

    Add initial HTML structure for the test page

---
+++ module_5/class_1/index.php
@@ -0,0 +1,10 @@
+<html>
+    <header>
+        <title>test</title>
+        <meta charset="UTF-8">
+    </header>
+    <body>
+        <h1>Test Page</h1>
+        <p>This is a test page to check the HTML structure.</p>
+    </body>
+</html>
